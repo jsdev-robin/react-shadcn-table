8b4d5fea8a911ac5652b61f069d29d3c7fc93c45
jsdev-robin <jsdev.robin@gmail.com>
2026-08-15 11:01:30 +0600

    feat: enhance HeaderMenu with pinning and visibility options for columns

--- src/package/ui/header/HeaderMenu.tsx
+++ src/package/ui/header/HeaderMenu.tsx
@@ -6,24 +6,33 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import type { GridFeatures } from '@/package/features';
+import { useGrid } from '@/package/hooks/useGrid';
 import { type Header, type RowData } from '@tanstack/react-table';
-import { ArrowDown, ArrowUp, EllipsisVertical } from 'lucide-react';
+import {
+  ArrowDown,
+  ArrowUp,
+  EllipsisVertical,
+  EyeOff,
+  PinIcon,
+  PinOff,
+} from 'lucide-react';
 
 const HeaderMenu = ({
   header,
 }: {
   header: Header<GridFeatures, RowData, unknown>;
 }) => {
-  'use no memo';
+  const { isLoading, isError } = useGrid();
 
   return header.column.getCanFilter() ? (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size="icon-xs" variant="ghost">
+        <Button size="icon-xs" variant="ghost" disabled={isLoading || isError}>
           <EllipsisVertical />
         </Button>
       </DropdownMenuTrigger>
@@ -42,13 +51,68 @@ const HeaderMenu = ({
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() => {
-              header.column.toggleSorting(false);
+              header.column.toggleSorting(true);
             }}
             disabled={!header.column.getCanSort()}
           >
             Sort DESC
             <DropdownMenuShortcut>
               <ArrowDown />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        {!header.isPlaceholder && header.column.getCanPin() && (
+          <DropdownMenuGroup>
+            {header.column.getIsPinned() !== 'start' && (
+              <DropdownMenuItem
+                onClick={() => {
+                  header.column.pin('start');
+                }}
+              >
+                Pin to left
+                <DropdownMenuShortcut>
+                  <PinIcon className="mun:rotate-45" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            )}
+            {header.column.getIsPinned() && (
+              <DropdownMenuItem
+                onClick={() => {
+                  header.column.pin(false);
+                }}
+              >
+                Unpin
+                <DropdownMenuShortcut>
+                  <PinOff />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            )}
+            {header.column.getIsPinned() !== 'end' && (
+              <DropdownMenuItem
+                onClick={() => {
+                  header.column.pin('end');
+                }}
+              >
+                Pin to right
+                <DropdownMenuShortcut>
+                  <PinIcon className="mun:-rotate-45" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuGroup>
+        )}
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            onClick={() => {
+              header.column.toggleVisibility(false);
+            }}
+            disabled={!header.column.getCanHide()}
+          >
+            Hide column
+            <DropdownMenuShortcut>
+              <EyeOff />
             </DropdownMenuShortcut>
           </DropdownMenuItem>
         </DropdownMenuGroup>
