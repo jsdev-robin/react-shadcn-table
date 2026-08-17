b5ddebba3acc4deaf9be97017d6518392687da26
jsdev-robin <jsdev.robin@gmail.com>
2026-08-17 13:40:12 +0600

    feat: update package.json and pnpm-lock.yaml to manage peer dependencies and clean up unused packages; modify vite.config.ts to include dts plugin and configure build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,41 @@
-import babel from '@rolldown/plugin-babel';
-import tailwindcss from '@tailwindcss/vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+// import babel from '@rolldown/plugin-babel';
+// import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
+    // babel({ presets: [reactCompilerPreset()] }),
+    // tailwindcss(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'react-shadcn-table',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rolldownOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
