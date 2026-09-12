cfb0ee38974fab53b86e1fd8ba46fe9490c19ecb
jsdev-robin <jsdev.robin@gmail.com>
2026-09-12 22:05:11 +0600

    refactor: remove unused components and styles
    - Deleted App.css, App.tsx, index.css, and main.tsx as they contained unused styles and components.
    - Updated vite.config.ts to remove unused plugins and added support for TypeScript declaration files.
    - Introduced new index.ts file to export package components.
    - Added useQueryArgs hook and updated package index to export necessary types and hooks.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,57 @@
-import babel from '@rolldown/plugin-babel';
-import tailwindcss from '@tailwindcss/vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import path from 'path';
+// import babel from '@rolldown/plugin-babel';
+// import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
+    // babel({ presets: [reactCompilerPreset()] }),
+    // tailwindcss(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
+      '@': resolve(import.meta.dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
+      name: 'react-shadcn-table',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rolldownOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        '@dnd-kit/core',
+        '@dnd-kit/sortable',
+        '@dnd-kit/modifiers',
+        '@dnd-kit/utilities',
+        '@tanstack/react-hotkeys',
+        '@tanstack/react-pacer',
+        '@tanstack/react-store',
+        '@tanstack/react-table',
+        'jspdf',
+        'jspdf-autotable',
+        'xlsx',
+        'radix-ui',
+        'lucide-react',
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
